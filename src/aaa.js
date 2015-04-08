2e269dab7fa09285f06bc2cd0779b9d1bae11ef2
Dominik Magiera <test1@o2.pl>
2015-04-08 11:19:18 +0200

    aaa

---
+++ src/aaa.js
@@ -0,0 +1,68 @@
+/**
+ * Created by student on 08.04.15.
+ */
+
+function main(meaning) {
+
+    var i=0;
+    var number = 4;
+    if (meaning === 42) {
+        if(number>5){
+            i=0;
+            while (i<4) {
+
+
+                if (true) {
+
+                }
+                i++;
+
+            }
+        }
+
+
+        if(number <5){
+            i=0;
+            while (i<4) {
+
+
+                if (true) {
+
+                }
+                i++;
+
+            }
+        }
+
+        if(number ===5){
+            i=0;
+            while (i<4) {
+
+
+                if (true) {
+
+                }
+                i++;
+
+            }
+        }
+        if(number ===35){
+            i=0;
+            while (i<4) {
+
+
+                if (true) {
+
+                }
+                i++;
+
+            }
+        }
+
+    }
+}
+
+
+
+
+
