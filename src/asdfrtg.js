449e9a12dbcbdc3ea6102053680b2543a7d280a1
Aleksander Furtak <Aleksander@furtak.pwsz>
2015-04-14 21:48:14 +0200

    s

---
+++ src/asdfrtg.js
@@ -0,0 +1,35 @@
+
+function a(){
+
+	if(true){
+		var x = 0;
+		while(x<10){
+			
+		x++;
+		}
+	}
+	
+
+}
+
+
+function test() {
+
+	if (true) {
+		a();
+	}
+	if (true) {
+		a();
+	}
+	if (true) {
+		a();
+	}
+	if (true) {
+		a();
+	}
+	if (true) {
+		a();
+	}
+
+	
+}
