b08ce01430260513f4f7025cac4df82651cb376c
Katruf <aleksy246@gmail.com>
2015-04-22 10:37:00 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,16 @@
+
 module.exports = function (grunt) {
-  'use strict';
-
-  grunt.initConfig({
-
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      }, all: {
-        src: ['src/**/*.js']
-      }
-    }
-
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-
+'use strict';
+grunt.initConfig({
+jshint: {
+options: {
+reporter: 'checkstyle',
+reporterOutput: 'target/checkstyle.xml',
+jshintrc: '.jshintrc'
+}, all: {
+src: ['src/**/*.js']
+}
+}
+});
+grunt.loadNpmTasks('grunt-contrib-jshint');
 };
